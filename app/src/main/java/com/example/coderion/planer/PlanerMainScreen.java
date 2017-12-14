2baada03b2d686918032f00bac6a745cc5b4e99e
Michał Dziewanowski <michal.dziewanowski@coderion.pl>
2017-12-14 12:06:24 +0100

    a

---
+++ app/src/main/java/com/example/coderion/planer/PlanerMainScreen.java
@@ -0,0 +1,18 @@
+package com.example.coderion.planer;
+
+import android.graphics.PorterDuff;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class PlanerMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planer_main_screen);
+    }
+}
